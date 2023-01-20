10659e1930319c3458c8a9a6c275f86086b064e2
Lassi Piironen <lsipii@kapsi.fi>
2023-01-20 12:11:11 +0200

    feat: infra build the dynamodb model using app definitions

--- infra/resources/DynamoDB.ts
+++ infra/resources/DynamoDB.ts
@@ -1,53 +1,55 @@
-import * as aws from '@pulumi/aws';
-import * as awsnative from '@pulumi/aws-native';
+import * as aws from "@pulumi/aws";
+import * as awsnative from "@pulumi/aws-native";
 
-export function createDynamoDBTable(
-  configuration: StackConfig,
-  lambdaFunctionExecRole: awsnative.iam.Role
-) {
-  const tableName = configuration.generateResourceName('StatusTable');
-  const statusTable = new aws.dynamodb.Table(tableName, {
-    name: tableName,
-    attributes: [{ name: 'statusKey', type: 'S' }],
-    hashKey: 'statusKey',
-    billingMode: 'PAY_PER_REQUEST',
+import StatusAdminUIModel from "../../src/data/models/StatusAdminUI";
+
+export function createDynamoDBTable(configuration: StackConfig, lambdaFunctionExecRole: awsnative.iam.Role) {
+  const tableInfo: any = {
+    name: StatusAdminUIModel.tableName,
+    attributes: StatusAdminUIModel.schema.AttributeDefinitions.map((ad) => {
+      return {
+        name: ad.AttributeName,
+        type: ad.AttributeType,
+      };
+    }),
+  };
+
+  for (const keySchema of StatusAdminUIModel.schema.KeySchema) {
+    if (keySchema.KeyType === "HASH") {
+      tableInfo.hashKey = keySchema.AttributeName;
+    } else if (keySchema.KeyType === "RANGE") {
+      tableInfo.rangeKey = keySchema.AttributeName;
+    }
+  }
+
+  const fulltableName = configuration.generateResourceName(tableInfo.name);
+  const statusTable = new aws.dynamodb.Table(fulltableName, {
+    ...tableInfo,
+    billingMode: "PAY_PER_REQUEST",
     tags: configuration.getTags(),
   });
 
-  const dynamoDBPolicy = new aws.iam.Policy(
-    configuration.generateResourceName('dynamoDBPolicy'),
-    {
-      description: 'DynamoDB policy for authentication-gw',
-      policy: statusTable.arn.apply(arn => {
-        return JSON.stringify({
-          Version: '2012-10-17',
-          Statement: [
-            {
-              Effect: 'Allow',
-              Action: [
-                'dynamodb:UpdateItem',
-                'dynamodb:GetItem',
-                'dynamodb:PutItem',
-                'dynamodb:DeleteItem',
-                'dynamodb:DescribeTable',
-                'dynamodb:Scan',
-              ],
-              Resource: [arn],
-            },
-          ],
-        });
-      }),
-    }
-  );
+  const dynamoDBPolicy = new aws.iam.Policy(configuration.generateResourceName("dynamoDBPolicy"), {
+    description: "DynamoDB policy for status-info api",
+    policy: statusTable.arn.apply((arn) => {
+      return JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Effect: "Allow",
+            Action: ["dynamodb:UpdateItem", "dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem", "dynamodb:DescribeTable", "dynamodb:Scan"],
+            Resource: [arn],
+          },
+        ],
+      });
+    }),
+  });
 
   // Attach to role
-  new aws.iam.RolePolicyAttachment(
-    configuration.generateResourceName('dynamoDBPolicyAttachment'),
-    {
-      role: lambdaFunctionExecRole.arn,
-      policyArn: dynamoDBPolicy.arn,
-    }
-  );
+  new aws.iam.RolePolicyAttachment(configuration.generateResourceName("dynamoDBPolicyAttachment"), {
+    role: lambdaFunctionExecRole.arn,
+    policyArn: dynamoDBPolicy.arn,
+  });
 
   return statusTable;
 }
