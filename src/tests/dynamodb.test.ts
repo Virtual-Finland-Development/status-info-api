b3355e95282b29fa95419cbd9920282873efca7d
Lassi Piironen <lsipii@kapsi.fi>
2023-01-23 12:00:13 +0200

    featt: add a tests suite

---
+++ src/tests/dynamodb.test.ts
@@ -0,0 +1,35 @@
+import { expect, it, vi } from "vitest";
+import DynamoDB from "../lib/AWS/DynamoDB/";
+
+it("should work", async () => {
+  vi.mock(
+    "@aws-sdk/client-dynamodb",
+    vi.fn(async () => {
+      const actual: any = await vi.importActual("@aws-sdk/client-dynamodb");
+      return {
+        ...actual,
+        DynamoDBClient: vi.fn(),
+      };
+    })
+  );
+  vi.mock(
+    "@aws-sdk/lib-dynamodb",
+    vi.fn(() => ({
+      DynamoDBDocumentClient: {
+        from: vi.fn(
+          vi.fn(() => {
+            return {
+              send: vi.fn(() => {
+                return {
+                  Items: [],
+                };
+              }),
+            };
+          })
+        ),
+      },
+    }))
+  );
+
+  expect(await DynamoDB.scan("StatusInfo")).toEqual([]); // Baseline test
+});
